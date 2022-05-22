d3fd31216a7eb078030088e30d56870658ee293e
Chen Larry <73185261+chen150298@users.noreply.github.com>
2022-05-22 14:43:49 +0300

    Add files via upload

---
+++ server-ASP.NET/Domain/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
