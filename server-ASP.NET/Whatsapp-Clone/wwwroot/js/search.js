74ec97566d6702f2f3598c0d794476bba225da59
Chen Larry <73185261+chen150298@users.noreply.github.com>
2022-05-22 15:21:06 +0300

    Add files via upload

---
+++ server-ASP.NET/Whatsapp-Clone/wwwroot/js/search.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    $('form').submit(async (e) => {
+        e.preventDefault();
+        const q = $('#search').val();
+
+        var response = await fetch('/Ratings/SearchResults?query=' + q);
+        var data = await response.json();
+        console.log(data);
+
+        const template = $('#template').html();
+        let results = '';
+        for (var item in data) {
+            let row = template;
+            for (var key in data[item]) {
+                row = row.replaceAll('{' + key + '}', data[item][key]);
+                row = row.replaceAll('%7B' + key + '%7D', data[item][key]);
+            }
+            results += row;
+        }
+        $('tbody').html(results);
+
+    })
+});
